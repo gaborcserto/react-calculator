61249192c4689977ef0508051bc89577569a0675
Gabor Cserto <csertog@gmail.com>
2020-09-11 12:38:08 +0200

    Updates

---
+++ precache-manifest.83bfd3ed287ee204b970f44bd04cf566.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42654e9f0db74a1f8438a6957a4ad03b",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "e596425d9d0b70c7e1aa",
+    "url": "/react-calculator/static/css/main.505d9719.chunk.css"
+  },
+  {
+    "revision": "8a8868bce3272a48fcd8",
+    "url": "/react-calculator/static/js/2.a7c2fef7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.a7c2fef7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e596425d9d0b70c7e1aa",
+    "url": "/react-calculator/static/js/main.b2effff9.chunk.js"
+  },
+  {
+    "revision": "3c4ee116adfa9d8605e9",
+    "url": "/react-calculator/static/js/runtime-main.5a7a9823.js"
+  }
+]);
